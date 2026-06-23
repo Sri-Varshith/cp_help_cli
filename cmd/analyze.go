84d8a07f33811f1b2443b447c631322a69dcf6aa
Sri Varshith <nvarshith.2007@gmail.com>
2026-06-23 17:03:11 +0530

    fixes for env variables

--- cmd/analyze.go
+++ cmd/analyze.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/Sri-Varshith/cp_help_cli/internal/ai"
 	"github.com/Sri-Varshith/cp_help_cli/internal/parser"
 	"github.com/spf13/cobra"
 )
@@ -17,12 +18,19 @@ var analyzeCmd = &cobra.Command{
 
 		code, err := parser.ReadFile(file)
 		if err != nil {
-			fmt.Printf("Error reading file: %v\n", err)
+			fmt.Println(err)
 			return
 		}
 
-		fmt.Printf("Successfully read %s\n", file)
-		fmt.Printf("Code length: %d bytes\n", len(code))
+		prompt := ai.BuildAnalyzePrompt(code)
+
+		response, err := ai.Ask(prompt)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(response)
 	},
 }
 
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/Sri-Varshith/cp_help_cli/cmd"
+import (
+	"github.com/Sri-Varshith/cp_help_cli/cmd"
+	"github.com/Sri-Varshith/cp_help_cli/internal/ai"
+)
 
 func main() {
+	ai.LoadEnv()
 	cmd.Execute()
 }
