516b3d2d1031a7b76e37e458432b9940de4882e9
Sri Varshith <nvarshith.2007@gmail.com>
2026-06-23 16:52:43 +0530

    file parser

---
+++ internal/parser/file.go
@@ -0,0 +1,12 @@
+package parser
+
+import "os"
+
+func ReadFile(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
